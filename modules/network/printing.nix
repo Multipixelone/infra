4bcdf9ca1d939645669083c75d7e35d267fef166
Multipixelone <finn@cnwr.net>
2026-03-24 13:05:56 -0400

    feat: ensure printer

--- modules/network/printing.nix
+++ modules/network/printing.nix
@@ -1,3 +1,21 @@
 {
-  flake.modules.nixos.pc.services.printing.enable = true;
+  flake.modules.nixos.pc = {
+    services.printing.enable = true;
+
+    hardware.printers = {
+      ensurePrinters = [
+        {
+          name = "Brother_HL_L2460DW";
+          description = "Brother HL-L2460DW";
+          deviceUri = "ipp://192.168.3.131/ipp/print";
+          model = "everywhere";
+          ppdOptions = {
+            PageSize = "Letter";
+            Duplex = "DuplexNoTumble"; # two-sided-long-edge
+          };
+        }
+      ];
+      ensureDefaultPrinter = "Brother_HL_L2460DW";
+    };
+  };
 }
