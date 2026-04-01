cf94b1b01164a4d1d716da38274417ce6b2088df
Multipixelone <finn@cnwr.net>
2026-04-01 16:07:30 -0400

    fix: assign the devShell a name

--- modules/devshell.nix
+++ modules/devshell.nix
@@ -1,4 +1,10 @@
 { inputs, ... }:
 {
   imports = [ inputs.make-shell.flakeModules.default ];
+
+  perSystem =
+    { config, ... }:
+    {
+      make-shells.default.name = config.flake.meta.repo.name;
+    };
 }
