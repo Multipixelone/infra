1761108939d329cc866560cd16cd219ca43c12e3
Multipixelone <finn@cnwr.net>
2026-04-01 20:01:38 -0400

    fix: descriptive bat package information

--- modules/shell/bat.nix
+++ modules/shell/bat.nix
@@ -26,11 +26,12 @@
             pager = "${lib.getExe pkgs.ov} --quit-if-one-screen --header 3";
           };
           extraPackages = with pkgs.bat-extras; [
-            prettybat
-            batwatch
-            batpipe
-            batman
-            batdiff
+            batman # Read system manual pages (man) using bat as the manual page formatter
+            batgrep # Quickly search through and highlight files using ripgrep
+            batdiff # Diff a file against the current git index, or display the diff between two files
+            batpipe # Less (and soon bat) preprocessor for viewing more types of files in the terminal
+            batwatch # Watch for changes in one or more files, and print them with bat
+            prettybat # Pretty-print source code
           ];
         };
       };
