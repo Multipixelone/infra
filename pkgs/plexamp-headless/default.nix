7b189156e0018a273bfe6d9a68a4dc6167614265
Multipixelone <finn@cnwr.net>
2026-04-02 21:47:18 -0400

    fix: pin node_20

--- pkgs/plexamp-headless/default.nix
+++ pkgs/plexamp-headless/default.nix
@@ -1,24 +1,31 @@
 {
   lib,
   stdenvNoCC,
-  nodejs,
+  fetchurl,
+  makeWrapper,
+  # treble.node is compiled for Node.js ABI v115 (node_register_module_v115)
+  nodejs_20,
 }:
-stdenvNoCC.mkDerivation {
+stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "plexamp-headless";
-  version = "4.13.0";
+  version = "4.12.4";
 
-  src = null;
-  dontUnpack = true;
+  src = fetchurl {
+    url = "https://plexamp.plex.tv/headless/Plexamp-Linux-headless-v${finalAttrs.version}.tar.bz2";
+    hash = "sha256-lXlNTpvYhCqNIev/76wHJRzNvzmxzpOnZEx61O2kt3I=";
+  };
+
+  nativeBuildInputs = [ makeWrapper ];
 
   installPhase = ''
     runHook preInstall
 
+    mkdir -p "$out/lib/plexamp-headless"
+    cp -r . "$out/lib/plexamp-headless/"
+
     mkdir -p "$out/bin"
-    cat > "$out/bin/plexamp-headless" <<'EOF'
-    #!/usr/bin/env sh
-    exec ${lib.getExe nodejs} /var/lib/plexamp-headless/plexamp/js/index.js "$@"
-    EOF
-    chmod +x "$out/bin/plexamp-headless"
+    makeWrapper "${lib.getExe nodejs_20}" "$out/bin/plexamp-headless" \
+      --add-flags "$out/lib/plexamp-headless/js/index.js"
 
     runHook postInstall
   '';
@@ -29,6 +36,6 @@ stdenvNoCC.mkDerivation {
     mainProgram = "plexamp-headless";
     license = lib.licenses.unfree;
     platforms = [ "x86_64-linux" ];
-    sourceProvenance = [ lib.sourceTypes.binaryNativeCode ];
+    sourceProvenance = [ lib.sourceTypes.binaryBytecode ];
   };
-}
+})
