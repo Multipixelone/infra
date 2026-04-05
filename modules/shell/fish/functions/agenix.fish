4387cc172b1a99278155b505e67bf89948b3c469
Multipixelone <finn@cnwr.net>
2026-04-05 09:28:39 -0400

    fix: don't always append identity flag

--- modules/shell/fish/functions/agenix.fish
+++ modules/shell/fish/functions/agenix.fish
@@ -1,3 +1,13 @@
 function agenix
+    if test (count $argv) -eq 0
+        command agenix --help
+        return
+    end
+
+    if contains -- -i $argv; or contains -- --identity $argv
+        command agenix $argv
+        return
+    end
+
     command agenix --identity $HOME/.ssh/agenix $argv
 end
