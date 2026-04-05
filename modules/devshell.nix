bd4c985ef42be7b0951f8c614bfe7b04bccd8ed6
Multipixelone <finn@cnwr.net>
2026-04-05 09:26:32 -0400

    fix: proper config scoping

--- modules/devshell.nix
+++ modules/devshell.nix
@@ -1,10 +1,8 @@
-{ inputs, ... }:
+{ inputs, config, ... }:
 {
   imports = [ inputs.make-shell.flakeModules.default ];
 
-  perSystem =
-    { config, ... }:
-    {
-      make-shells.default.name = config.flake.meta.repo.name;
-    };
+  perSystem = {
+    make-shells.default.name = config.flake.meta.repo.name;
+  };
 }
