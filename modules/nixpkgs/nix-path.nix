2e51b8a3fd9f4c50ebd4a866aced3def9941df8c
Multipixelone <finn@cnwr.net>
2026-04-08 13:53:51 -0400

    fix: disable channels

--- modules/nixpkgs/nix-path.nix
+++ modules/nixpkgs/nix-path.nix
@@ -6,14 +6,11 @@
         flakeInputs = lib.filterAttrs (_: v: lib.isType "flake" v) inputs;
       in
       {
+        channel.enable = false;
         registry = lib.mapAttrs (_: v: { flake = v; }) flakeInputs;
         nixPath = [
           "nixpkgs=${inputs.nixpkgs}"
         ];
       };
-  };
-
-  flake.modules.homeManager.base = {
-    nix.nixPath = [ "nixpkgs=${inputs.nixpkgs}" ];
   };
 }
