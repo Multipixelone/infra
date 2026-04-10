7845e863d599caa6335b374dfa78c3ddd9449e33
Multipixelone <finn@cnwr.net>
2026-04-09 20:15:34 -0400

    refactor: break out make-shell input

--- modules/devshell.nix
+++ modules/devshell.nix
@@ -1,5 +1,9 @@
 { inputs, config, ... }:
 {
+  flake-file.inputs.make-shell = {
+    url = "github:nicknovitski/make-shell";
+    inputs.flake-compat.follows = "flake-compat";
+  };
   imports = [ inputs.make-shell.flakeModules.default ];
 
   perSystem = {
--- modules/flake-file.nix
+++ modules/flake-file.nix
@@ -102,11 +102,6 @@
         inputs.nixpkgs.follows = "nixpkgs";
       };
 
-      make-shell = {
-        url = "github:nicknovitski/make-shell";
-        inputs.flake-compat.follows = "flake-compat";
-      };
-
       blocklist = {
         url = "github:StevenBlack/hosts";
         flake = false;
