75510068219192dfe242d83684feb1227d63bbc5
Multipixelone <finn@cnwr.net>
2026-04-11 21:10:26 -0400

    fix: move catppu & stylix definitions to gui module

--- modules/shell/ai/opencode.nix
+++ modules/shell/ai/opencode.nix
@@ -3,24 +3,27 @@
   ...
 }:
 {
-  flake.modules.homeManager.base =
-    _:
-    let
-      aiConfig = config.flake.aiConfig;
-    in
-    {
+  flake.modules.homeManager = {
+    gui = {
       stylix.targets.opencode.enable = false;
       catppuccin.opencode.enable = false;
-      programs.opencode = {
-        enable = true;
-        enableMcpIntegration = true;
-        agents = aiConfig.agentsDir;
-        skills = aiConfig.skillsDir;
-        rules = aiConfig.rulesText;
-        settings = {
-          model = "anthropic/claude-sonnet-4-5";
-          autoupdate = false;
+    };
+    base =
+      let
+        aiConfig = config.flake.aiConfig;
+      in
+      {
+        programs.opencode = {
+          enable = true;
+          enableMcpIntegration = true;
+          agents = aiConfig.agentsDir;
+          skills = aiConfig.skillsDir;
+          rules = aiConfig.rulesText;
+          settings = {
+            model = "anthropic/claude-sonnet-4-5";
+            autoupdate = false;
+          };
         };
       };
-    };
+  };
 }
