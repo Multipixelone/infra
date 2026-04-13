304bc62252c89ca8842cd40474026bd76a057c46
Multipixelone <finn@cnwr.net>
2026-04-13 00:20:45 -0400

    feat: gitleaks pre-commit hook

--- modules/git/hooks.nix
+++ modules/git/hooks.nix
@@ -13,9 +13,19 @@
   gitignore = [ "/.pre-commit-config.yaml" ];
 
   perSystem =
-    { config, ... }:
+    { config, pkgs, ... }:
     {
-      make-shells.default.shellHook = config.pre-commit.installationScript;
+      make-shells.default = {
+        shellHook = config.pre-commit.installationScript;
+        packages = [ pkgs.gitleaks ];
+      };
       pre-commit.check.enable = false;
+      pre-commit.settings.hooks.gitleaks = {
+        enable = true;
+        name = "gitleaks";
+        entry = "gitleaks git --pre-commit --redact --staged --verbose";
+        pass_filenames = false;
+        language = "system";
+      };
     };
 }
