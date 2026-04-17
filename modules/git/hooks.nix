d1961652fe568ab516be83164cd0fd2f3b4a41bb
Multipixelone <finn@cnwr.net>
2026-04-17 09:24:38 -0400

    feat: more pre-commit hooks to cleanup files

--- modules/git/hooks.nix
+++ modules/git/hooks.nix
@@ -16,16 +16,28 @@
     { config, pkgs, ... }:
     {
       make-shells.default = {
+        inputsFrom = [
+          config.pre-commit.devShell
+        ];
         shellHook = config.pre-commit.installationScript;
         packages = [ pkgs.gitleaks ];
       };
       pre-commit.check.enable = false;
-      pre-commit.settings.hooks.gitleaks = {
-        enable = true;
-        name = "gitleaks";
-        entry = "gitleaks git --pre-commit --redact --staged --verbose";
-        pass_filenames = false;
-        language = "system";
+      pre-commit.settings.hooks = {
+        # General use pre-commit hooks
+        trim-trailing-whitespace.enable = true;
+        mixed-line-endings.enable = true;
+        end-of-file-fixer.enable = true;
+        check-executables-have-shebangs.enable = true;
+        check-added-large-files.enable = true;
+        # git secret checking
+        gitleaks = {
+          enable = true;
+          name = "gitleaks";
+          entry = "gitleaks git --pre-commit --redact --staged --verbose";
+          pass_filenames = false;
+          language = "system";
+        };
       };
     };
 }
