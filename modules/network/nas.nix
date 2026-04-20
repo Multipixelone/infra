da148a410cbbd0af179a71f29c5f5dfe23c27c7a
Multipixelone <finn@cnwr.net>
2026-04-19 23:06:43 -0400

    feat: add media nfs mount

--- modules/network/nas.nix
+++ modules/network/nas.nix
@@ -1,6 +1,7 @@
 # FIXME https://github.com/NixOS/nixpkgs/issues/370185
 {
   flake.modules.nixos.pc = {
+    boot.supportedFilesystems = [ "nfs" ];
     fileSystems."/media/alexandria" = {
       device = "192.168.6.9:/volume1/homes/tunnel";
       options = [
@@ -12,6 +13,16 @@
       fsType = "nfs";
 
     };
-    boot.supportedFilesystems = [ "nfs" ];
+    fileSystems."/media/media" = {
+      device = "192.168.6.9:/volume1/Media";
+      options = [
+        "nfsvers=4.1"
+        "x-systemd.automount"
+        "noauto"
+      ];
+
+      fsType = "nfs";
+
+    };
   };
 }
