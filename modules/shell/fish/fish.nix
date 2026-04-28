86e423b5c4bdf3592c662770952b88b3503a3aa9
Multipixelone <finn@cnwr.net>
2026-04-28 18:36:04 -0400

    fix: set default shell to fish

--- modules/shell/fish/fish.nix
+++ modules/shell/fish/fish.nix
@@ -6,15 +6,7 @@
       {
         programs.fish.enable = true;
         programs.command-not-found.enable = false;
-        programs.bash = {
-          interactiveShellInit = ''
-            if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-            then
-              shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-              exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
-            fi
-          '';
-        };
+        users.defaultUserShell = pkgs.fish;
       };
     homeManager.base =
       {
