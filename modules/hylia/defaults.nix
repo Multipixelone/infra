b6add1e85e7224c2ca6f91e4d1380ad9f0b0dcb8
Finn Rutis <me@finnrut.is>
2026-06-29 14:41:22 -0400

    feat: nicer default settings

--- modules/hylia/defaults.nix
+++ modules/hylia/defaults.nix
@@ -7,13 +7,17 @@
         show-recents = false;
         mru-spaces = false;
         tilesize = 48;
+        wvous-br-corner = 14; # bottom-right hot corner → Quick Note
       };
       finder = {
         AppleShowAllExtensions = true;
         FXEnableExtensionChangeWarning = false;
         ShowPathbar = true;
+        ShowStatusBar = true;
         _FXShowPosixPathInTitle = true;
         FXPreferredViewStyle = "Nlsv";
+        FXDefaultSearchScope = "SCcf"; # search the current folder, not the whole Mac
+        _FXSortFoldersFirst = true;
       };
       NSGlobalDomain = {
         AppleInterfaceStyle = "Dark";
@@ -23,6 +27,19 @@
         "com.apple.keyboard.fnState" = true;
         NSAutomaticCapitalizationEnabled = false;
         NSAutomaticSpellingCorrectionEnabled = false;
+        NSAutomaticPeriodSubstitutionEnabled = false;
+        NSNavPanelExpandedStateForSaveMode = true; # always-expanded save dialog
+        PMPrintingExpandedStateForPrint = true; # always-expanded print dialog
+      };
+      menuExtraClock = {
+        Show24Hour = true;
+        ShowDayOfWeek = true;
+        ShowDate = 0; # 0 = when space allows
+      };
+      WindowManager.HideDesktop = true; # hide items in Stage Manager
+      screencapture = {
+        location = "~/Pictures/Screenshots";
+        type = "png";
       };
       loginwindow.GuestEnabled = false;
       trackpad.Clicking = true;
