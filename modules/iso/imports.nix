bff192851cc06caec4dbce6baa422a9d3767bb82
Multipixelone <finn@cnwr.net>
2026-07-27 21:21:11 -0400

    fix: supportedFilesystems on iso

--- modules/iso/imports.nix
+++ modules/iso/imports.nix
@@ -1,4 +1,9 @@
-{ inputs, config, ... }:
+{
+  inputs,
+  config,
+  lib,
+  ...
+}:
 {
   flake-file.inputs.nixos-generators = {
     url = "github:nix-community/nixos-generators";
@@ -7,6 +12,12 @@
 
   configurations.nixos.iso.module = {
     hardware.graphics.enable = true;
+    # installation-cd-graphical-calamares-plasma6.nix pulls in ZFS support
+    # for installer media. zfs-kernel is currently marked broken against
+    # this nixpkgs pin's kernel, which aborts eval outright - disable it the
+    # same way upstream's own *-no-zfs.nix installer variants do, since this
+    # repo has no ZFS hosts to install onto anyway.
+    boot.supportedFilesystems.zfs = lib.mkForce false;
     imports = with config.flake.modules.nixos; [
       "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-calamares-plasma6.nix"
       "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/channel.nix"
